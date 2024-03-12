34dcbeb014405195e23ce9f83e71cf86fea6b911
P3TR0z <52086073+P3TR0z@users.noreply.github.com>
2024-03-12 20:47:28 +0200

    Create CreateVenv.ps1

---
+++ build/CreateVenv.ps1
@@ -0,0 +1,15 @@
+$envName = "py_virtual_env'
+$fullEnvPath = "$PSScriptRoot/$envName"
+$activateScriptPath = "$fullEnvPath/Scripts/Active.ps1"
+
+Out-Host -InputObject("Creating python venv: $fullEnvPath")
+python.exe -m venv $fullEnvPath
+
+& $activateScriptPath
+
+#upgrades
+python -m pip install --upgrade pip
+pip list
+deactivate
+
+Out-Host -InputObject("exit code: {0}" -f $LASTEXITCODE)
